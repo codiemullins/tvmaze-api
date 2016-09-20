61e68eaa8e17eeb5462a6b8c2fd36cde169df4af
Codie Mullins <codie.mullins@waterfield.com>
2016-09-20 17:08:22 -0500

    Finish tests for Show and add Travis CI

--- lib/tvmaze/api/show.rb
+++ lib/tvmaze/api/show.rb
@@ -1,13 +1,14 @@
 require "json"
-require "awesome_print"
 
 module Tvmaze
   module Api
     class Show
-      attr_reader :id, :url, :name, :type, :language, :genres, :status, :summary
+      attr_reader(
+        :id, :url, :name, :type, :language, :genres, :status, :summary, :runtime, :premiered, :schedule,
+        :rating, :externals, :image, :links, :updated
+      )
 
       def initialize attributes
-        ap attributes
         @id = attributes["id"]
         @url = attributes["url"]
         @name = attributes["name"]
@@ -16,6 +17,14 @@ module Tvmaze
         @genres = attributes["genres"]
         @status = attributes["status"]
         @summary = attributes["summary"]
+        @runtime = attributes["runtime"]
+        @premiered = Date.parse attributes["premiered"]
+        @schedule = attributes["schedule"]
+        @rating = attributes["rating"]["average"]
+        @externals = attributes["externals"]
+        @image = attributes["image"]
+        @links = attributes["_links"]
+        @updated = Time.at attributes["updated"]
       end
 
       def self.find id
--- spec/tvmaze/api/show_spec.rb
+++ spec/tvmaze/api/show_spec.rb
@@ -10,8 +10,17 @@ VCR.use_cassette 'single_show' do
       is_expected.to have_attributes(
         id: 4,
         name: "Arrow",
+        status: "Running",
+        runtime: a_kind_of(Fixnum),
         genres: a_kind_of(Array),
-        summary: a_kind_of(String)
+        summary: a_kind_of(String),
+        premiered: Date.parse('2012-10-10'),
+        schedule: a_kind_of(Hash),
+        rating: 7.9,
+        externals: a_kind_of(Hash),
+        image: a_kind_of(Hash),
+        links: a_kind_of(Hash),
+        updated: a_kind_of(Time),
       )
     end
 
